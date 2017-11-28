b218978a7656b48eadfdd3c9d56619ffb91c3a44
Adrian Lara <adrianblara@gmail.com>
2017-11-28 13:44:57 -0700

    use module instead of namespace in items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,15 @@
-class Api::V1::ItemsController < ApplicationController
+module Api
+  module V1
+    class ItemsController < ApplicationController
 
-  def index
-    render json: Item.all
+      def index
+        render json: Item.all
+      end
+
+      def show
+        render json: Item.find(params[:id])
+      end
+
+    end
   end
-
-  def show
-    render json: Item.find(params[:id])
-  end
-
 end
