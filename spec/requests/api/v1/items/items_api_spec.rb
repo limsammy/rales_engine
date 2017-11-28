5c5b820667190561af94be5eb09bb334a3d5fa7c
Adrian Lara <adrianblara@gmail.com>
2017-11-27 23:58:53 -0700

    factory and test written for all items endpoint

---
+++ spec/requests/api/v1/items/items_api_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "Items API" do
+  before(:each) do
+    create_list(:item, 5)
+  end
+
+  it "sends a list of items" do
+    get '/api/v1/items'
+
+    expect(response).to be_success
+
+    items = JSON.parse(response.body)
+    item = items.first
+
+    expect(items.count).to eq(5)
+    expect(item).to have_key("id")
+    expect(item).to have_key("name")
+    expect(item).to have_key("description")
+    expect(item).to have_key("unit_price")
+    expect(item).to have_key("merchant_id")
+    expect(item).to have_key("created_at")
+    expect(item).to have_key("updated_at")
+  end
+end
