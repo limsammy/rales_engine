389de1df7206bd05529a78d30161bdf01e87bcd0
limsammy1@gmail.com <limsammy1@gmail.com>
2017-11-28 10:06:40 -0700

    Added show, find, and find_all routes to route file and created find_controller and customers_controller. Tests still not fully passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :customers, only: [:index]
+      namespace :customers do
+        get '/find', to: "find#show"
+        get '/find_all', to: "find#index"
+      end
+      resources :customers, only: [:index, :show]
     end
   end
 end
