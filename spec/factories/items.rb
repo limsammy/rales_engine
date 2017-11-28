6d626f62529a0568f9c4ed6fb5a33b197a216432
Sam Lim <limsammy1@gmail.com>
2017-11-28 13:56:28 -0700

    Revert "Item endpoints"

--- spec/factories/items.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :item do
-    sequence(:id) { |n| n }
-    sequence(:name) { |n| "Name #{n}" }
-    sequence(:description) { |n| "Description #{n}" }
-    sequence(:unit_price) { |n| 111 * n}
-    sequence(:merchant_id) { |n| 100 * n }
-  end
-end
