94572b69d597bdad96d68913f6d10763772e7602
Adrian Lara <adrianblara@gmail.com>
2017-11-28 00:18:31 -0700

    test written for individual item endpoint

--- spec/requests/api/v1/items/items_api_spec.rb
+++ spec/requests/api/v1/items/items_api_spec.rb
@@ -22,4 +22,20 @@ describe "Items API" do
     expect(item).to have_key("created_at")
     expect(item).to have_key("updated_at")
   end
+
+  it "sends an individual item" do
+    get "/api/v1/items/#{Item.first.id}"
+
+    expect(response).to be_success
+
+    item = JSON.parse(response.body)
+
+    expect(item).to have_key("id")
+    expect(item).to have_key("name")
+    expect(item).to have_key("description")
+    expect(item).to have_key("unit_price")
+    expect(item).to have_key("merchant_id")
+    expect(item).to have_key("created_at")
+    expect(item).to have_key("updated_at")
+  end
 end
