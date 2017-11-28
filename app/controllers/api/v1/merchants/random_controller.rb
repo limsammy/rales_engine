39b98e5c5460840bf51304ddb8b9b19e8ff5952d
limsammy1@gmail.com <limsammy1@gmail.com>
2017-11-28 13:14:17 -0700

    Added random controller

---
+++ app/controllers/api/v1/merchants/random_controller.rb
@@ -0,0 +1,13 @@
+module Api
+  module V1
+    module Merchants
+      class RandomController < ApplicationController
+
+        def show
+          render json: Merchant.pluck.sample(1)
+        end
+
+      end
+    end
+  end
+end
