d5b907e235313a63222506b996abf6b160a0fe4d
limsammy1@gmail.com <limsammy1@gmail.com>
2017-11-29 14:21:37 -0700

    Fixed query

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,7 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
-  has_many :customers
+  has_many :customers, through: :invoices
 
   def favorite_customer
     customers.joins(:transactions)
