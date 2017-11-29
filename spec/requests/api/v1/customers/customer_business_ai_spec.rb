77ba9ceebcc1a8f12e76ad65f61947e1b3a48ca4
limsammy1@gmail.com <limsammy1@gmail.com>
2017-11-29 14:41:02 -0700

    Wrote customer business ai spec

---
+++ spec/requests/api/v1/customers/customer_business_ai_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "Customer Business Intelligence" do
+  it "returns the merchant who has conducted the most total number of successful transactions" do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    merchant2 = create(:merchant)
+    item = create(:item, merchant_id: merchant.id, unit_price: 100)
+
+    invoice1 = create(:invoice, customer_id: customer.id, merchant_id: merchant.id)
+    invoice_item1 = create(:invoice_item, invoice_id: invoice1.id, quantity: 2, item_id: item.id)
+    transaction1 = create(:transaction, invoice_id: invoice1.id, result: "success")
+    invoice2 = create(:invoice, customer_id: customer.id, merchant_id: merchant.id)
+    invoice_item2 = create(:invoice_item, invoice_id: invoice2.id, quantity: 2, item_id: item.id)
+    transaction2 = create(:transaction, invoice_id: invoice2.id, result: "success")
+
+    invoice3 = create(:invoice, customer_id: customer.id, merchant_id: merchant2.id)
+    invoice_item3 = create(:invoice_item, invoice_id: invoice3.id, quantity: 2, item_id: item.id)
+    transaction1 = create(:transaction, invoice_id: invoice3.id, result: "success")
+
+    get "/api/v1/merchants/#{merchant.id}/favorite_customer"
+
+    fav_merchant = JSON.parse(response.body)
+    expect(fav_merchant["id"]).to eq(merchant.id)
+  end
+end
