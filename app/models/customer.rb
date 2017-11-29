e2a209976572d5adf6a148c2ed210f7066e706b4
limsammy1@gmail.com <limsammy1@gmail.com>
2017-11-29 16:52:57 -0700

    Added has_many through relationship for transactions through invoices and wrote favorite_merchant method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,13 @@
 class Customer < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
+  has_many :merchants, through: :invoices
 
   def favorite_merchant
-    
+    merchants.joins(invoices: :transactions)
+      .merge(Transaction.where(result: 'success'))
+      .group('merchants.id')
+      .order('count(merchants.id) DESC')
+      .first
   end
 end
