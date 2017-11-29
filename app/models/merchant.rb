6b0835e420b7e71222b685bd0e070750b2b20ab9
limsammy1@gmail.com <limsammy1@gmail.com>
2017-11-29 14:05:36 -0700

    Set up association has_many customers and wrote favorite_customer method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,13 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+  has_many :customers
+
+  def favorite_customer
+    customers.joins(:transactions)
+      .merge(Transaction.where(result: 'success'))
+      .group('customers.id')
+      .order('count(customers.id) DESC')
+      .first
+  end
 end
