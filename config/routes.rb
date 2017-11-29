42bf5db4b838fca5cee7c00ad548c0a8ce3c58b5
limsammy1@gmail.com <limsammy1@gmail.com>
2017-11-28 23:40:26 -0700

    added routes for customer relationships

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
         get '/find', to: "find#show"
         get '/find_all', to: "find#index"
         get '/random', to: "random#show"
+        get '/:id/invoices', to: "invoices#index"
+        get '/:id/transactions', to: "transactions#index"
       end
       resources :customers, only: [:index, :show]
 
