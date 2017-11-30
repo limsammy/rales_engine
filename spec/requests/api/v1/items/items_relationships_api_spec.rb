a71d79caf56ea025208899db5b208d3768170a78
Adrian Lara <adrianblara@gmail.com>
2017-11-30 14:45:59 -0700

    item most revenue written and tested

--- spec/requests/api/v1/items/items_relationships_api_spec.rb
+++ spec/requests/api/v1/items/items_relationships_api_spec.rb
@@ -7,7 +7,15 @@ describe "Item Relationship API" do
     @item = create(:item, merchant_id: @merchant.id)
     @invoices = create_list(:invoice, 5, merchant_id: @merchant.id, customer_id: @customer.id)
     @transaction = create(:transaction, invoice_id: @invoices.first.id)
-    @invoice_item = create(:invoice_item, item_id: @item.id, invoice_id: @invoices.first.id)
+    @invoice_item = create(:invoice_item, item_id: @item.id, invoice_id: @invoices.first.id, unit_price: 1, quantity: 1)
+
+    @items = create_list(:item, 3, merchant_id: @merchant.id)
+    @invoice_item2 = create(:invoice_item, item_id: @items.first.id, invoice_id: @invoices.first.id, unit_price: 200, quantity: 2)
+    @invoice_item3 = create(:invoice_item, item_id: @items.second.id, invoice_id: @invoices.second.id, unit_price: 100, quantity: 1)
+    @invoice_item4 = create(:invoice_item, item_id: @items.third.id, invoice_id: @invoices.third.id, unit_price: 300, quantity: 3)
+    @trx1 = create(:transaction, invoice_id: @invoices.first.id, result: "success")
+    @trx2 = create(:transaction, invoice_id: @invoices.second.id, result: "success")
+    @trx3 = create(:transaction, invoice_id: @invoices.third.id, result: "success")
   end
 
   it "can return a collection of associated invoice_items" do
@@ -28,5 +36,17 @@ describe "Item Relationship API" do
     merchant = JSON.parse(response.body)
 
     expect(merchant["name"]).to eq(@merchant.name)
+  end
+
+  it "can return top x items by revenue" do
+    get "/api/v1/items/most_revenue?quantity=2"
+
+    expect(response).to be_success
+
+    items = JSON.parse(response.body)
+
+    expect(items.count).to eq(2)
+    expect(items.first["name"]).to eq(@items.third.name)
+    expect(items.second["name"]).to eq(@items.first.name)
   end
 end
