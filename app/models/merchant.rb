6989486b54fd966e14009f17f6b88f72bb271e0f
limsammy1@gmail.com <limsammy1@gmail.com>
2017-11-29 19:38:20 -0700

    Wrote class method most_revenue, added has_many relations for transactions and invoice items

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,12 +2,22 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
   has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
+  has_many :invoice_items, through: :invoices
 
   def favorite_customer
     customers.joins(:transactions)
-      .merge(Transaction.where(result: 'success'))
-      .group('customers.id')
-      .order('count(customers.id) DESC')
-      .first
+    .merge(Transaction.where(result: 'success'))
+    .group('customers.id')
+    .order('count(customers.id) DESC')
+    .first
+  end
+
+  def self.most_revenue(quantity)
+    unscoped.select("merchants.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS total_revenue")
+      .joins(:transactions, :invoice_items)
+      .group(:id)
+      .order("total_revenue DESC")
+      .limit(quantity)
   end
 end
