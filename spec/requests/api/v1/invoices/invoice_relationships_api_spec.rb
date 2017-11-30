d5860b014cc4db58b840e6ae4be9a5cdfc69a6b7
Adrian Lara <adrianblara@gmail.com>
2017-11-29 18:01:50 -0700

    invoice invoice_items endpoints written and tested

--- spec/requests/api/v1/invoices/invoice_relationships_api_spec.rb
+++ spec/requests/api/v1/invoices/invoice_relationships_api_spec.rb
@@ -4,8 +4,10 @@ describe "Invoice Relationships API" do
   before(:each) do
     @merchant = create(:merchant)
     @customer = create(:customer)
+    @item = create(:item, merchant_id: @merchant.id)
     @invoices = create_list(:invoice, 5, merchant_id: @merchant.id, customer_id: @customer.id)
     @transaction = create(:transaction, invoice_id: @invoices.first.id)
+    @invoice_item = create(:invoice_item, item_id: @item.id, invoice_id: @invoices.first.id)
   end
 
   it "can return a collection of associated transactions" do
@@ -16,6 +18,16 @@ describe "Invoice Relationships API" do
     transactions = JSON.parse(response.body)
 
     expect(transactions.first["id"]).to eq(@transaction.id)
+  end
+
+  it "can return a collection of associated invoice items" do
+    get "/api/v1/invoices/#{@invoices.first.id}/invoice_items"
+
+    expect(response).to be_success
+
+    associated_invoice_items = JSON.parse(response.body)
+
+    expect(associated_invoice_items.first["id"]).to eq(@invoice_item.id)
   end
 
 end
