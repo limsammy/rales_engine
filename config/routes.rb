affb0a0c9ef24cbaaac6ebce1e347464d38274ce
Adrian Lara <adrianblara@gmail.com>
2017-11-29 18:01:50 -0700

    invoice invoice_items endpoints written and tested

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
 
       namespace :invoices do
         get '/:id/transactions', to: "transactions#index"
+        get '/:id/invoice_items', to: "invoice_items#index"
       end
       resources :invoices, only: [:index, :show]
 
