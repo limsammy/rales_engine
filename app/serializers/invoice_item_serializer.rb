f2c697fc2f76f8c2ae95e2524e16f1e6c14526db
Adrian Lara <adrianblara@gmail.com>
2017-11-30 10:25:45 -0700

    serializers added to invoice and invoice item

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,3 +1,8 @@
 class InvoiceItemSerializer < ActiveModel::Serializer
   attributes :id, :item_id, :invoice_id, :quantity, :unit_price
+
+  def unit_price
+    sprintf("%.2f",object.unit_price/100.0)
+  end
+
 end
