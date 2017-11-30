ac8a9e68e17cb80b55aaf4551039802248f0add3
Adrian Lara <adrianblara@gmail.com>
2017-11-29 23:20:57 -0700

    invoice_item item endpoint written and tested

---
+++ spec/requests/api/v1/invoice_items/invoice_items_relationships_api_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "Invoice Items Relationships API" do
+  before(:each) do
+    @merchant = create(:merchant)
+    @customer = create(:customer)
+    @item = create(:item, merchant_id: @merchant.id)
+    @invoices = create_list(:invoice, 5, merchant_id: @merchant.id, customer_id: @customer.id)
+    @transaction = create(:transaction, invoice_id: @invoices.first.id)
+    @invoice_item = create(:invoice_item, item_id: @item.id, invoice_id: @invoices.first.id)
+  end
+
+  it "can return associated item" do
+    get "/api/v1/invoice_items/#{@invoice_item.id}/item"
+
+    expect(response).to be_success
+
+    item = JSON.parse(response.body)
+
+    expect(item["name"]).to eq(@item.name)
+  end
+end
