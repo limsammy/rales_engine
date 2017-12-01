5cb791d9bfbf2b5b9b54d5ff7be2581ba39288c3
Adrian Lara <adrianblara@gmail.com>
2017-11-30 20:47:18 -0700

    all invoice items can be found endpoint written and tested

--- spec/requests/api/v1/invoice_items/invoice_items_api_spec.rb
+++ spec/requests/api/v1/invoice_items/invoice_items_api_spec.rb
@@ -5,9 +5,9 @@ describe "Invoice Items API" do
   before(:each) do
     merchant_id = create(:merchant).id
     customer_id = create(:customer).id
-    item = create(:item, merchant_id: merchant_id)
-    invoice = create(:invoice, merchant_id: merchant_id, customer_id: customer_id)
-    create_list(:invoice_item, 5, item_id: item.id, invoice_id: invoice.id)
+    @item = create(:item, merchant_id: merchant_id)
+    @invoice = create(:invoice, merchant_id: merchant_id, customer_id: customer_id)
+    create_list(:invoice_item, 5, item_id: @item.id, invoice_id: @invoice.id)
   end
 
   it "returns all invoice items" do
@@ -46,5 +46,18 @@ describe "Invoice Items API" do
     invoice_item = JSON.parse(response.body)
 
     expect(invoice_item["id"]).to eq(inv_item.id)
+  end
+
+  it "can find all instances of a given unit_price" do
+    inv_item = InvoiceItem.second
+    create(:invoice_item, item_id: @item.id, invoice_id: @invoice.id, unit_price: inv_item.unit_price)
+    get "/api/v1/invoice_items/find_all?unit_price=#{inv_item.unit_price/100.0}"
+
+    expect(response).to be_success
+
+    invoice_items = JSON.parse(response.body)
+
+    expect(invoice_items.count).to eq(2)
+    expect(invoice_items.first["id"]).to eq(inv_item.id)
   end
 end
